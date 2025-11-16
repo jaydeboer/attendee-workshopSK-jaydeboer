da64e0f2de89605d5139e3fd41289dab57507428
Jay DeBoer <jaydeboer@gmail.com>
2025-11-16 19:54:07 +0000

    Lab 4.1 complete

--- exercises/module4/start/ChatWithAgent.cs
+++ exercises/module4/start/ChatWithAgent.cs
@@ -21,10 +21,45 @@ internal class ChatWithAgent
 {
     public async Task LetAgentFindRide(IConfiguration config)
     {
-        Console.WriteLine("******** Create the agent ***********");
-        Console.WriteLine("******** Start the agent ***********");
-        Console.WriteLine("******** RESPONSE ***********"); 
+        var question =
+        """
+        I stay at the WestIn Seattle and the venue is the Seattle Kraken Stadium.
+        The concert starts at 7:30 pm and is November 20th this year. 
+        """;
+
+        var agent = CreateTransportationAgent(config);
+
+        var agentresult = agent.InvokeAsync(question);
+        Console.WriteLine("******** RESPONSE ***********");
+        await PrintResult(agentresult);
     }
+    private ChatCompletionAgent CreateTransportationAgent(IConfiguration config)
+    {
+        var kernel = CreateKernel(config);
+
+        var instructions = """
+            You are an expert in finding transportation options from a given hotel location to the concert location.
+            You will try to get the best options available for an affordable price. Make sure the customer will be there at least 30 minutes before the concert starts at the venue.
+            You always suggest 3 options with different price ranges.
+            You will ask for approval before you make the booking.
+            """;
+
+        ChatCompletionAgent transportationAgent =
+            new()
+            {
+                Name = "TransportationAgent",
+                Instructions = instructions,
+                Description = "An agent that finds transportation options from hotel to concert location",
+                Kernel = kernel,
+                LoggerFactory = LoggerFactory.Create(builder =>
+                    {
+                        // Add Console logging provider
+                        builder.AddConsole().SetMinimumLevel(LogLevel.Trace);
+                    })
+            };
+        return transportationAgent;
+    }
+
 
     private static Kernel CreateKernel(IConfiguration config)
     {
@@ -38,5 +73,17 @@ internal class ChatWithAgent
 
         var kernel = kernelBuilder.Build();
         return kernel;
+    }
+
+    private static async Task PrintResult(IAsyncEnumerable<AgentResponseItem<ChatMessageContent>> agentResponse)
+    {
+        await
+        foreach (var item in agentResponse)
+        {
+            Console.WriteLine($"Thread: {item.Thread.Id}");
+            Console.WriteLine($"Thread data: {item.Thread}");
+            Console.WriteLine($"Author: {item.Message.AuthorName}");
+            Console.WriteLine($"Message:{item.Message}");
+        }
     }
 }
